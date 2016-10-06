96f4bef3ec0e62283f831c3fe284b2d038ed22c1
Jared Welch <jarwelch@bellsouth.net>
2016-10-07 10:50:55 +1100

    Uses Rails.logger

--- lib/mandrill.rb
+++ lib/mandrill.rb
@@ -33,7 +33,7 @@ module Mandrill
             params[:key] = @apikey
             params = JSON.generate(params)
             r = @session.post(:path => "#{@path}#{url}.json", :headers => {'Content-Type' => 'application/json'}, :body => params)
-            p "Response: #{r}"
+            Rails.logger.debug "Headers: #{r.headers}"
             cast_error(r.body) if r.status != 200
             return JSON.parse(r.body)
         end
